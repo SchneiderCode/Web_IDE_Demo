ef509505fe38bbb1394b802234c3021ba0bf7d91
Michael Joseph Schneider <misc7570@colorado.edu>
2020-09-01 21:33:14 -0600

    Start

---
+++ precache-manifest.8ea721fda0ade2c65c62b4c274aff002.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55e787b51d545c5046f7df5190bfb72",
+    "url": "./index.html"
+  },
+  {
+    "revision": "882b3f45803764640267",
+    "url": "./static/css/main.05a437e3.chunk.css"
+  },
+  {
+    "revision": "f72fa7fceced052fd958",
+    "url": "./static/js/2.98aff9d7.chunk.js"
+  },
+  {
+    "revision": "46b054eece7ec568f940a92b676d3f2b",
+    "url": "./static/js/2.98aff9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882b3f45803764640267",
+    "url": "./static/js/main.e0c2f27c.chunk.js"
+  },
+  {
+    "revision": "6f8317c1b6ccb3c09ce4",
+    "url": "./static/js/runtime-main.6b75c0ae.js"
+  },
+  {
+    "revision": "1be099a0a72f9294beba590075abb5b6",
+    "url": "./static/media/connect.1be099a0.gif"
+  },
+  {
+    "revision": "0b759de5b1a4dcd00f9b1e573cfdc217",
+    "url": "./static/media/control_summary.0b759de5.gif"
+  },
+  {
+    "revision": "343a10c461e30bbee70bc7833c72ac47",
+    "url": "./static/media/disconnect.343a10c4.gif"
+  },
+  {
+    "revision": "d62421d764c40d749255d261dd1f862d",
+    "url": "./static/media/instructable1.d62421d7.png"
+  },
+  {
+    "revision": "bb65e50898ada2c58ebc042f54ef748d",
+    "url": "./static/media/instructable2.bb65e508.png"
+  }
+]);
